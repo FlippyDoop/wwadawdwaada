7aed73d4779785001aa411abedf0a03b3c754797
FlippyDoop <75170122+FlippyDoop@users.noreply.github.com>
2021-04-15 14:08:53 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,44 @@
+var canvas, backgroundImage;
+
+var gameState = 0;
+var playerCount;
+var allPlayers;
+var distance = 0;
+var database;
+var car1,car2,car3,car4,cars;
+var c1,c2,c3,c4, track , ground;
+
+var form, player, game;
+
+function Preload(){
+  c1 = loadImage("images/car1.png");
+  c2 = loadImage("images/car2.png");
+  c3 = loadImage("images/car3.png");
+  c4 = loadImage("images/car4.png");
+ track = loadImage("track.jpg.png");  
+ ground = loadImage("images/ground.png");
+
+}
+
+function setup(){
+  canvas = createCanvas(displayWidth -30,displayHeight-150);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
+}
+
+
+function draw(){
+  if(playerCount === 4){
+    game.update(1);
+  }
+  if(gameState === 1){
+    clear();
+    game.play();
+  }
+
+  if(gameState === 2){
+    game.end();
+  }
+}
